71f5238aaafb1d5007e56c1cc0b59c5d5090dcdb
n.yoshida <inazuma074@gmail.com>
2022-10-04 23:27:18 +0900

    [update] add Test page

---
+++ src/views/Test.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="test">
+        <h1>test</h1>
+        <p class="msg">Hello, World!</p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped lang="scss">
+.test {
+    .msg {
+        font-weight: bold;
+    }
+}
+</style>
