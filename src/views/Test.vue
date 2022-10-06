c71f8bf6114014ab36a4c574fe3eb7a1f5b19780
n.yoshida <inazuma074@gmail.com>
2022-10-06 23:12:22 +0900

    [update] Test page

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,15 +1,35 @@
+<script setup>
+    import {onMounted, ref} from 'vue'
+
+    const msg = ref('Hello!')
+    const count = ref(0)
+    const check = ref(false)
+    const list = ['hoge', 'fuga', 'piyo']
+
+    function log() {
+        console.log(msg)
+    }
+
+    console.log('created')
+
+    onMounted(() => {
+        console.log('on mount');
+    })
+</script>
+
 <template>
     <div class="test">
-        <h1>test</h1>
-        <p class="msg">Hello, World!</p>
+        <p><input type="text" v-model="msg"></p>
+        <p class="msg">{{msg}}</p>
+        <input type="checkbox" v-model="check" />
+        <span v-if="check">true</span>
+        <span v-else>false</span>
+        <template v-for="(item, index) in list" :key="index">
+            <p>{{item}}</p>
+        </template>
+        <p><button @click="count++">{{count}}</button></p>
     </div>
 </template>
-
-<script>
-export default {
-    
-}
-</script>
 
 <style scoped lang="scss">
 .test {
